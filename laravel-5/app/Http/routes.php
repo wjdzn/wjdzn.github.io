e6363bb11a82cadc892b5c60d992eefc365388db
jfllopiz87 <jfllopiz87@gmail.com>
2015-04-23 17:58:24 -0400

    laravel 5

--- laravel-5/app/Http/routes.php
+++ laravel-5/app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::get('cart', 'CartController@index');
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
