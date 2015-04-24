72bb4dce4d30a43f7464c06a90c6f30c452662fc
jfllopiz87 <jfllopiz87@gmail.com>
2015-04-24 10:46:43 -0400

    add roles to user class

--- laravel-5/app/User.php
+++ laravel-5/app/User.php
@@ -6,9 +6,9 @@ use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract,HasRoleAndPermissionContract  {
 
-	use Authenticatable, CanResetPassword;
+	use Authenticatable, CanResetPassword, HasRoleAndPermission;
 
 	/**
 	 * The database table used by the model.
