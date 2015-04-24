e7bc0418094c44f621cf6e677745651ee37d6d4f
jfllopiz87 <jfllopiz87@gmail.com>
2015-04-24 11:40:51 -0400

    test cart

--- laravel-5/resources/views/cart/index.blade.php
+++ laravel-5/resources/views/cart/index.blade.php
@@ -1,28 +1,44 @@
-<table>
-    <thead>
-    <tr>
-        <th>Product</th>
-        <th>Qty</th>
-        <th>Item Price</th>
-        <th>Subtotal</th>
-    </tr>
-    </thead>
+@extends('app')
 
-    <tbody>
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Home</div>
 
-    <?php foreach($cart as $row) :?>
+                    <div class="panel-body">
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>Product</th>
+                                <th>Qty</th>
+                                <th>Item Price</th>
+                                <th>Subtotal</th>
+                            </tr>
+                            </thead>
 
-    <tr>
-        <td>
-            <p><strong><?php echo $row->name;?></strong></p>
-            <p><?php echo ($row->options->has('size') ? $row->options->size : '');?></p>
-        </td>
-        <td><input type="text" value="<?php echo $row->qty;?>"></td>
-        <td>$<?php echo $row->price;?></td>
-        <td>$<?php echo $row->subtotal;?></td>
-    </tr>
+                            <tbody>
 
-    <?php endforeach;?>
+                            <?php foreach($cart as $row) :?>
 
-    </tbody>
-</table>
+                            <tr>
+                                <td>
+                                    <p><strong><?php echo $row->name;?></strong></p>
+                                    <p><?php echo ($row->options->has('size') ? $row->options->size : '');?></p>
+                                </td>
+                                <td><input type="text" value="<?php echo $row->qty;?>"></td>
+                                <td>$<?php echo $row->price;?></td>
+                                <td>$<?php echo $row->subtotal;?></td>
+                            </tr>
+
+                            <?php endforeach;?>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
