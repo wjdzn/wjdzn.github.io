ab686a1e00e57e03e3487fd6f1a3c90a00bae864
jfllopiz87 <jfllopiz87@gmail.com>
2015-04-25 12:28:00 -0400

    admin view and controller

--- laravel-5/resources/views/admin/layouts/default.blade.php
+++ laravel-5/resources/views/admin/layouts/default.blade.php
@@ -238,26 +238,26 @@
                 <div class="page-sidebar  sidebar-nav">
                     <div class="nav_icons">
                         <ul class="sidebar_threeicons">
-                            <li>
-                                <a href="{{ URL::to('admin/form_builder') }}">
-                                    <i class="livicon" data-name="hammer" title="Form Builder 1" data-loop="true" data-color="#42aaca" data-hc="#42aaca" data-s="25"></i>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ URL::to('admin/form_builder2') }}">
-                                    <i class="livicon" data-name="list-ul" title="Form Builder 2" data-loop="true" data-color="#e9573f" data-hc="#e9573f" data-s="25"></i>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ URL::to('admin/buttonbuilder') }}">
-                                    <i class="livicon" data-name="vector-square" title="Button Builder" data-loop="true" data-color="#f6bb42" data-hc="#f6bb42" data-s="25"></i>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ URL::to('admin/gridmanager') }}">
-                                    <i class="livicon" data-name="new-window" title="Form Builder 1" data-loop="true" data-color="#37bc9b" data-hc="#37bc9b" data-s="25"></i>
-                                </a>
-                            </li>
+                            {{--<li>--}}
+                                {{--<a href="{{ URL::to('admin/form_builder') }}">--}}
+                                    {{--<i class="livicon" data-name="hammer" title="Form Builder 1" data-loop="true" data-color="#42aaca" data-hc="#42aaca" data-s="25"></i>--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
+                            {{--<li>--}}
+                                {{--<a href="{{ URL::to('admin/form_builder2') }}">--}}
+                                    {{--<i class="livicon" data-name="list-ul" title="Form Builder 2" data-loop="true" data-color="#e9573f" data-hc="#e9573f" data-s="25"></i>--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
+                            {{--<li>--}}
+                                {{--<a href="{{ URL::to('admin/buttonbuilder') }}">--}}
+                                    {{--<i class="livicon" data-name="vector-square" title="Button Builder" data-loop="true" data-color="#f6bb42" data-hc="#f6bb42" data-s="25"></i>--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
+                            {{--<li>--}}
+                                {{--<a href="{{ URL::to('admin/gridmanager') }}">--}}
+                                    {{--<i class="livicon" data-name="new-window" title="Form Builder 1" data-loop="true" data-color="#37bc9b" data-hc="#37bc9b" data-s="25"></i>--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
                         </ul>
                     </div>
                     <div class="clearfix"></div>
