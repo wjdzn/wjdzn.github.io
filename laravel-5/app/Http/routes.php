c439bbdf74b1dc84c2a9dd95e19ee3ae54bc658d
jfllopiz87 <jfllopiz87@gmail.com>
2015-04-25 13:06:03 -0400

    working with calendar in admin

--- laravel-5/app/Http/routes.php
+++ laravel-5/app/Http/routes.php
@@ -21,8 +21,9 @@ Route::get('cart', 'CartController@index');
 
 
 /* Admin Routes */
+Route::get('admin','AdminController@index');
 Route::group(array('prefix' => 'admin'), function () {
-    Route::get('/','AdminController@index');
+
     Route::get('calendar','AdminController@calendar');
 });
 
