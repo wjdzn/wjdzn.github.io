fb264e936cf2e2dfb88b916de40db56abedb1ff7
jfllopiz87 <jfllopiz87@gmail.com>
2015-04-25 11:28:54 -0400

    middlewate to admin controller

--- laravel-5/app/Http/Controllers/AdminController.php
+++ laravel-5/app/Http/Controllers/AdminController.php
@@ -20,7 +20,7 @@ class AdminController extends Controller {
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('admin');
     }
 
     /**
