083c5190f6b26d4c41dab7028a99511e7ae61287
jfllopiz87 <jfllopiz87@gmail.com>
2015-04-25 12:05:57 -0400

    admin view and controller

--- laravel-5/resources/views/admin/index.blade.php
+++ laravel-5/resources/views/admin/index.blade.php
@@ -9,11 +9,11 @@ Dashboard
 {{-- page level styles --}}
 @section('header_styles')
 <!--page level css -->
-<link href="{{ asset('assets/vendors/fullcalendar/css/fullcalendar.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets/css/pages/calendar_custom.css') }}" rel="stylesheet" type="text/css" />
-<link rel="stylesheet" media="all" href="{{ asset('assets/vendors/jvectormap/jquery-jvectormap.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/vendors/animate/animate.min.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/only_dashboard.css') }}" />
+<link href="{{ asset('/plugins/AdminJhon/vendors/fullcalendar/css/fullcalendar.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('/plugins/AdminJhon/css/pages/calendar_custom.css') }}" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" media="all" href="{{ asset('/plugins/AdminJhon/vendors/jvectormap/jquery-jvectormap.css') }}" />
+<link rel="stylesheet" href="{{ asset('/plugins/AdminJhon/vendors/animate/animate.min.css') }}">
+<link rel="stylesheet" href="{{ asset('/plugins/AdminJhon/css/only_dashboard.css') }}" />
 <!--end of page level css-->
 @stop
 
@@ -510,23 +510,23 @@ Dashboard
 {{-- page level scripts --}}
 @section('footer_scripts')
 <!--  todolist-->
-<script src="{{ asset('assets/js/todolist.js') }}"></script>
+<script src="{{ asset('/plugins/AdminJhon/js/todolist.js') }}"></script>
 <!-- EASY PIE CHART JS -->
-<script src="{{ asset('assets/vendors/charts/easypiechart.min.js') }}"></script>
-<script src="{{ asset('assets/vendors/charts/jquery.easypiechart.min.js') }}"></script>
+<script src="{{ asset('/plugins/AdminJhon/vendors/charts/easypiechart.min.js') }}"></script>
+<script src="{{ asset('/plugins/AdminJhon/vendors/charts/jquery.easypiechart.min.js') }}"></script>
 <!--for calendar-->
-<script src="{{ asset('assets/vendors/fullcalendar/fullcalendar.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/fullcalendar/calendarcustom.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('/plugins/AdminJhon/vendors/fullcalendar/fullcalendar.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('/plugins/AdminJhon/vendors/fullcalendar/calendarcustom.min.js') }}" type="text/javascript"></script>
 <!--   Realtime Server Load  -->
-<script src="{{ asset('assets/vendors/charts/jquery.flot.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/charts/jquery.flot.resize.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('/plugins/AdminJhon/vendors/charts/jquery.flot.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('/plugins/AdminJhon/vendors/charts/jquery.flot.resize.min.js') }}" type="text/javascript"></script>
 <!--Sparkline Chart-->
-<script src="{{ asset('assets/vendors/charts/jquery.sparkline.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/vendors/countUp/countUp.js') }}"></script>
+<script src="{{ asset('/plugins/AdminJhon/vendors/charts/jquery.sparkline.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/plugins/AdminJhon/vendors/countUp/countUp.js') }}"></script>
 <!--   maps -->
-<script src="{{ asset('assets/vendors/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
-<script src="{{ asset('assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
-<script src="{{ asset('assets/js/dashboard.js') }}" type="text/javascript"></script>
+<script src="{{ asset('/plugins/AdminJhon/vendors/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+<script src="{{ asset('/plugins/AdminJhon/vendors/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+<script src="{{ asset('/plugins/AdminJhon/js/dashboard.js') }}" type="text/javascript"></script>
 <!-- end of page level js -->
 <script type="text/javascript">
 $(document).ready(function() {
