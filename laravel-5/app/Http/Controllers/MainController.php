b2c8975ed982900b05436c8f0056fcfb5e127553
PSUAndre <andre@ownmyinvention.com>
2015-04-26 17:32:55 -0400

    controller change

--- laravel-5/app/Http/Controllers/MainController.php
+++ laravel-5/app/Http/Controllers/MainController.php
@@ -1,10 +1,5 @@
 <?php namespace App\Http\Controllers;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-
-use Illuminate\Http\Request;
-
 class MainController extends Controller {
 
 	/**
