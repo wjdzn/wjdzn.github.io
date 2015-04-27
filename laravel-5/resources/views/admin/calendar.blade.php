6d5438f9810d2d59128f58ebae4d38d3378ce4fa
jfllopiz87 <jfllopiz87@gmail.com>
2015-04-27 10:44:09 -0400

    load events from DB in calendar view

--- laravel-5/resources/views/admin/calendar.blade.php
+++ laravel-5/resources/views/admin/calendar.blade.php
@@ -163,7 +163,7 @@ Calendar
         var data={_token:  $('meta[name="csrf-token"]').attr('content')}
         $.ajax({
             type: "POST",
-            url: "{{route('calendar/save')}}",
+            url: "{{route('calendar/events')}}",
             data: data,
             success: function (retrib) {
                 var events = retrib;
