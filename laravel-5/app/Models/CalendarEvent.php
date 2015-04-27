657812a753445b8c62a63b2b38c0b1696e8e72ae
jfllopiz87 <jfllopiz87@gmail.com>
2015-04-27 09:46:57 -0400

    add column backgroundColor to calendar_events Model

--- laravel-5/app/Models/CalendarEvent.php
+++ laravel-5/app/Models/CalendarEvent.php
@@ -20,7 +20,7 @@ class CalendarEvent extends Model
      *
      * @var array
      */
-    protected $fillable = ['name', 'description', 'init_at','end_at'];
+    protected $fillable = ['name', 'description', 'init_at','end_at','backgroundcolor'];
 
     /**
      * Create a new Eloquent model instance.
