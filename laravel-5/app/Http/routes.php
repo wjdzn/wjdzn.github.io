ca32fccfbc58e67ba25fe688123b7468dd91b6ec
jfllopiz87 <jfllopiz87@gmail.com>
2015-04-27 10:08:49 -0400

    load events from DB in calendar view

--- laravel-5/app/Http/routes.php
+++ laravel-5/app/Http/routes.php
@@ -23,11 +23,11 @@ Route::get('cart', 'CartController@index');
 
 /* Admin Routes */
 Route::get('admin','AdminController@index');
-Route::post('calendar/get_events',array('as' => 'calendar/get_events', 'uses' => 'AdminController@get_events'));
 Route::group(array('prefix' => 'admin'), function () {
 
     Route::get('calendar','AdminController@calendar');
     Route::post('calendar/save',array('as' => 'calendar/save', 'uses' => 'AdminController@save_event'));
+    Route::post('calendar/events',array('as' => 'calendar/events', 'uses' => 'AdminController@get_events'));
 });
 
 
--- laravel-5/resources/views/admin/calendar.blade.php
+++ laravel-5/resources/views/admin/calendar.blade.php
@@ -163,7 +163,7 @@ Calendar
         var data={_token:  $('meta[name="csrf-token"]').attr('content')}
         $.ajax({
             type: "POST",
-            url: "{{route('calendar/get_events')}}",
+            url: "{{route('calendar/events')}}",
             data: data,
             success: function (retrib) {
                 var events = retrib;
