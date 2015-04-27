d91424303b40b300cdb29db6f894422da9d1292b
jfllopiz87 <jfllopiz87@gmail.com>
2015-04-27 18:06:57 -0400

    create route to forum

--- laravel-5/app/Http/routes.php
+++ laravel-5/app/Http/routes.php
@@ -22,11 +22,7 @@ Route::get('cart', 'CartController@index');
 
 
 Route::get('forum', function() {
-
-    Route::get('/', function() {
-        return Redirect::to('https://www.inventpalooza.com/forum/public');
-    });
-
+    return Redirect::to('https://www.inventpalooza.com/forum/public');
 });
 
 /* Admin Routes */
