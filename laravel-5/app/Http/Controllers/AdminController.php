b1848c1bfe7a3ffedf18e225d05ff774d656ddb4
jfllopiz87 <jfllopiz87@gmail.com>
2015-04-28 11:54:16 -0400

    working with dates in calendar

--- laravel-5/app/Http/Controllers/AdminController.php
+++ laravel-5/app/Http/Controllers/AdminController.php
@@ -55,8 +55,8 @@ class AdminController extends Controller {
     {
         $event = new CalendarEvent(Input::all());
 
-        $date_init=date('Y:m:d h:i:s',date(strtotime(Input::get('init_at'))));
-        $date_end =date('Y:m:d h:i:s', date(strtotime(Input::get('endt_at'))));
+        $date_init=date('Y:m:d h:i:s',strtotime(Input::get('init_at')));//date('Y:m:d h:i:s',date(strtotime(Input::get('init_at'))));
+        $date_end =date('Y:m:d h:i:s',strtotime(Input::get('endt_at')));//date('Y:m:d h:i:s', date(strtotime(Input::get('endt_at'))));
         $event->init_at = $date_init;
         $event->end_at = $date_end;
         $eventsLikeThisByName = CalendarEvent::where('name','=',$event->name)->where('all_day','=','1')->get();
