d2019710a485d55b0cb3f14692fd3387d13647a4
Alain <alain.lpa@gmail.com>
2015-04-29 13:54:40 -0400

    save an user in 2 tables

--- forum/app/controllers/UserController.php
+++ forum/app/controllers/UserController.php
@@ -64,7 +64,7 @@ class UserController extends BaseController
                         ));
 
                         //insert Laravel-5 table;
-                        $pass = \bcrypt(e(Input::get('password')));
+                        $pass = md5(e(Input::get('password')));
                         DB::insert('insert into users (name,email,password) values (?, ?)', array($name,$email,$pass));
 
                         $user = User::where('email', e(Input::get('email')))->first();
@@ -92,7 +92,7 @@ class UserController extends BaseController
                         ));
 
                         //insert Laravel-5 table;
-                        $pass = bcrypt(e(Input::get('password')));
+                        $pass = md5(e(Input::get('password')));
                         DB::insert('insert into users (name,email,password) values (?, ?)', array($name,$email,$pass));
 
                         $user = User::where('email', e(Input::get('email')))->first();
