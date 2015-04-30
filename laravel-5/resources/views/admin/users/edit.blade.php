cc745d58735c3af4c7ef0fea7509fb3697aa050f
jfllopiz87 <jfllopiz87@gmail.com>
2015-04-30 12:29:55 -0400

    edit user view

--- laravel-5/resources/views/admin/users/edit.blade.php
+++ laravel-5/resources/views/admin/users/edit.blade.php
@@ -217,23 +217,23 @@ Edit User
                                     <p class="text-danger"><strong>Be careful with group selection, if you give admin access.. they can access admin section</strong></p>
                                     <div class="form-group">
                                         <label for="group" class="col-sm-2 control-label">Group *</label>
-                                        {{--<div class="col-sm-10">--}}
-                                            {{--<select class="form-control " title="Select group..." name="groups[]" id="groups" required>--}}
-                                                {{--<option value="">Select</option>--}}
-                                                {{--@foreach($roles as $role)--}}
-                                                    {{--<option value="{{{ $role->id }}}" {{($user_local->is($role) ? ' selected="selected"' : '') }}>{{ $role->name }}</option>--}}
-                                                {{--@endforeach--}}
-                                            {{--</select>--}}
-                                        {{--</div>--}}
+                                        <div class="col-sm-10">
+                                            <select class="form-control " title="Select group..." name="groups[]" id="groups" required>
+                                                <option value="">Select</option>
+                                                @foreach($roles as $role)
+                                                    <option value="{{{ $role->id }}}" {{($user_local->is($role) ? ' selected="selected"' : '') }}>{{ $role->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
                                     </div>
 
-                                    <div class="form-group">
-                                        <label for="activate" class="col-sm-2 control-label"> Activate User</label>
-                                        <div class="col-sm-10">
-                                            <input id="activate" name="activate" type="checkbox" class="pos-rel p-l-30" value="1" @if(Input::old('activate', $user->isActivated())) checked="checked" @endif  >
-                                        </div>
-                                        <span>If user is not activated, mail will be sent to user with activation link</span>
-                                    </div>
+                                    {{--<div class="form-group">--}}
+                                        {{--<label for="activate" class="col-sm-2 control-label"> Activate User</label>--}}
+                                        {{--<div class="col-sm-10">--}}
+                                            {{--<input id="activate" name="activate" type="checkbox" class="pos-rel p-l-30" value="1" @if(Input::old('activate', $user->isActivated())) checked="checked" @endif  >--}}
+                                        {{--</div>--}}
+                                        {{--<span>If user is not activated, mail will be sent to user with activation link</span>--}}
+                                    {{--</div>--}}
                                     
                                 
                                 </section>
