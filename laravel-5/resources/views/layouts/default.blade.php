299b207b70d8128906dfa287bea95aa8b2c6bdb1
jfllopiz87 <jfllopiz87@gmail.com>
2015-05-08 10:52:45 -0400

    comment prices box

--- laravel-5/resources/views/layouts/default.blade.php
+++ laravel-5/resources/views/layouts/default.blade.php
@@ -208,84 +208,84 @@
 
     <div class="amazing_style">
         <div class="container">
-            <div class="sixteen columns">
-                <div class="gstyle">
-                    <div class="t1_style">Our Services Prices and Plans</div>
-                    <div class="t2_style">Our service is totaly thin and light.</div>
-                    <div class="t3_style">Lorem ipsum dolor sit amet consectetur  enim ad minim veniam quis nostrud.</div>
-                </div>
+            {{--<div class="sixteen columns">--}}
+                {{--<div class="gstyle">--}}
+                    {{--<div class="t1_style">Our Services Prices and Plans</div>--}}
+                    {{--<div class="t2_style">Our service is totaly thin and light.</div>--}}
+                    {{--<div class="t3_style">Lorem ipsum dolor sit amet consectetur  enim ad minim veniam quis nostrud.</div>--}}
+                {{--</div>--}}
 
 
-                <div class="one-third column  alpha">
-                    <div class="th1_style">
-                        <div class="a_bloc1_style">
-                            <p class="a1_style">Free</p>
-                            <p class="a2_style">Get All The Freebies</p>
-                        </div>
+                {{--<div class="one-third column  alpha">--}}
+                    {{--<div class="th1_style">--}}
+                        {{--<div class="a_bloc1_style">--}}
+                            {{--<p class="a1_style">Free</p>--}}
+                            {{--<p class="a2_style">Get All The Freebies</p>--}}
+                        {{--</div>--}}
 
-                        <br>
-                        <p class="a3_style">5 Projects</p>
-                        <p class="a3_style">1 GB Storage</p>
-                        <p class="a3_style">No Domain</p>
-                        <p class="a3_style">1 User</p>
-                        <p class="a3_style"><span class="fsc1">0$</span><sup>/m</sup> </p>
+                        {{--<br>--}}
+                        {{--<p class="a3_style">5 Projects</p>--}}
+                        {{--<p class="a3_style">1 GB Storage</p>--}}
+                        {{--<p class="a3_style">No Domain</p>--}}
+                        {{--<p class="a3_style">1 User</p>--}}
+                        {{--<p class="a3_style"><span class="fsc1">0$</span><sup>/m</sup> </p>--}}
 
-                        <div class="a_bloc3_style">
-                            <div class="subscribe_st">
+                        {{--<div class="a_bloc3_style">--}}
+                            {{--<div class="subscribe_st">--}}
 
-                                <span class="subscribe_a"> <a href="#" class="slow_fade">Subscribe</a> </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                                {{--<span class="subscribe_a"> <a href="#" class="slow_fade">Subscribe</a> </span>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
 
-                <div class="one-third column ">
-                    <div class="th2_style">
-                        <div class="b_bloc1_style">
-                            <p class="b1_style">Personal</p>
-                            <p class="b2_style">Get All The Features</p>
-                        </div>
+                {{--<div class="one-third column ">--}}
+                    {{--<div class="th2_style">--}}
+                        {{--<div class="b_bloc1_style">--}}
+                            {{--<p class="b1_style">Personal</p>--}}
+                            {{--<p class="b2_style">Get All The Features</p>--}}
+                        {{--</div>--}}
 
-                        <br>
-                        <p class="b3_style">25 Projects</p>
-                        <p class="b3_style">50 GB Storage</p>
-                        <p class="b3_style">with 1 Domain</p>
-                        <p class="b3_style">10 Users</p>
-                        <p class="b3_style"><span class="fsc2">9,99$</span><sup>/m</sup> </p>
+                        {{--<br>--}}
+                        {{--<p class="b3_style">25 Projects</p>--}}
+                        {{--<p class="b3_style">50 GB Storage</p>--}}
+                        {{--<p class="b3_style">with 1 Domain</p>--}}
+                        {{--<p class="b3_style">10 Users</p>--}}
+                        {{--<p class="b3_style"><span class="fsc2">9,99$</span><sup>/m</sup> </p>--}}
 
-                        <div class="b_bloc3_style">
-                            <div class="subscribe_st">
+                        {{--<div class="b_bloc3_style">--}}
+                            {{--<div class="subscribe_st">--}}
 
-                                <span class="subscribe_b"> <a href="#" class="slow_fade">Subscribe</a> </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                                {{--<span class="subscribe_b"> <a href="#" class="slow_fade">Subscribe</a> </span>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
 
-                <div class="one-third column   omega">
-                    <div class="th1_style">
-                        <div class="a_bloc1_style">
-                            <p class="a1_style">Company</p>
-                            <p class="a2_style">Get All The Features</p>
-                        </div>
+                {{--<div class="one-third column   omega">--}}
+                    {{--<div class="th1_style">--}}
+                        {{--<div class="a_bloc1_style">--}}
+                            {{--<p class="a1_style">Company</p>--}}
+                            {{--<p class="a2_style">Get All The Features</p>--}}
+                        {{--</div>--}}
 
-                        <br>
-                        <p class="a3_style">100 Projects</p>
-                        <p class="a3_style">250 GB Storage</p>
-                        <p class="a3_style">Unlimited Domains</p>
-                        <p class="a3_style">50 Users</p>
-                        <p class="a3_style"><span class="fsc1">70$</span><sup>/m</sup> </p>
+                        {{--<br>--}}
+                        {{--<p class="a3_style">100 Projects</p>--}}
+                        {{--<p class="a3_style">250 GB Storage</p>--}}
+                        {{--<p class="a3_style">Unlimited Domains</p>--}}
+                        {{--<p class="a3_style">50 Users</p>--}}
+                        {{--<p class="a3_style"><span class="fsc1">70$</span><sup>/m</sup> </p>--}}
 
-                        <div class="a_bloc3_style">
-                            <div class="subscribe_st">
-                                <span class="subscribe_a"> <a href="#" class="slow_fade">Subscribe</a> </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                        {{--<div class="a_bloc3_style">--}}
+                            {{--<div class="subscribe_st">--}}
+                                {{--<span class="subscribe_a"> <a href="#" class="slow_fade">Subscribe</a> </span>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
 
 
-            </div>
+            {{--</div>--}}
         </div><!-- container -->
     </div>
 
