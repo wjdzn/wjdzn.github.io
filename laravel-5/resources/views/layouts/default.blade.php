fd3095fcab9ce353ac7b263185dd5d672fb2a805
jfllopiz87 <jfllopiz87@gmail.com>
2015-05-09 14:02:47 -0400

    adding meta csr-token in default.blade

--- laravel-5/resources/views/layouts/default.blade.php
+++ laravel-5/resources/views/layouts/default.blade.php
@@ -11,7 +11,7 @@
     <title>@yield('title')</title>
     <meta name="description" content="">
     <meta name="author" content="">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <!-- Mobile Specific Metas
   ================================================== -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
